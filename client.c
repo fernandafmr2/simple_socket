38fa11641e68f6928fd966cfb2f9a63a80d863c9
akiekano <hello.akiekano@gmail.com>
2022-05-31 00:29:40 +0700

    v2

--- client.c
+++ client.c
@@ -1,36 +1,33 @@
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<stdio.h>
-#include<netdb.h>
-#include<unistd.h>
-#include<strings.h>
-#include<string.h>
-#include<stdlib.h>
-#include<stdarg.h>
-#include<errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdarg.h>
+
+#include <arpa/inet.h>
+#include <unistd.h>
 
 // http port
 #define SERVER_PORT 80
 
 // buffer data
 #define MAXLINE 4096
-#define SA struct sockaddr
 
 void err_die(const char *fmt, ...);
 
 int main(int argc, char **argv)
 {
+	if (argc != 2)
+		err_die("usage: %s <server addr>", argv[0]);
+
 	int  		sockfd, n;
-	int  		sendbytes;
+	size_t  	sendbytes;
 	struct sockaddr_in  	serveraddr;
 	char 		sendline[MAXLINE];
 	char 		recvline[MAXLINE];
 
-	if (argc != 2)
-		err_die("usage: %s <server addr>", argv[0]);
-
-	// AF_INET = address family-internet
-	// 0 = use tcp
+	// SOCK_STREAM = use tcp
+	// SOCK_DGRAM = use udp
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		err_die("Error when create socket");
 
@@ -38,30 +35,35 @@ int main(int argc, char **argv)
 	serveraddr.sin_family 	= AF_INET;
 	serveraddr.sin_port		= htons(SERVER_PORT); // host network, short
 	
-	if(inet_pton(AF_INET, argv[1], &serveraddr.sin_addr) <= 0)
+	if(inet_pton(AF_INET, argv[1], &serveraddr.sin_addr) <= 0){
+		close(sockfd);
 		err_die("inet_pton err for %s ", argv[1]);
+	}
 
 	// connect server
-	if(connect(sockfd, (SA *) &serveraddr, sizeof(serveraddr)) < 0)
-		err_die("connect failed");
+	if(connect(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0){
+		close(sockfd);
+		err_die("connect failed");		
+	}
 
 	sprintf(sendline, "GET / HTTP/1.1\r\n\r\n");
 	sendbytes = strlen(sendline);
 
 	// send req
-	if(write(sockfd, sendline, sendbytes) != sendbytes)
+	if(write(sockfd, sendline, sendbytes) < 0)
 		err_die("write err");
 
 	// receive response
 	memset(recvline, 0, MAXLINE);
 	while((n = read(sockfd, recvline, MAXLINE-1)) > 0)
 	{
-		printf("%s", recvline);
-		memset(recvline, 0, MAXLINE);
+		recvline[n] = 0x00;
+		printf("block read: \n<%s>\n", recvline);
 	}
 	if(n<0)
 		err_die("read error");
 
+	close(sockfd);
 	exit(0);
 }
 
@@ -79,9 +81,8 @@ void err_die(const char *fmt, ...)
 
 	if (errno_save != 0)
 	{
-		fprintf(stdout, "(errno = %d) : %s\n", errno_save,
+		fprintf(stdout, "(errno = %d) : %s\n\n", errno_save,
 		strerror(errno_save));
-		fprintf(stdout, "\n");
 		fflush(stdout);
 	}
 	va_end(ap);
